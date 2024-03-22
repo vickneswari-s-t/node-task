90d29fefeb4dd9c6d083623fc4c9b32f866536ae
vickneswari-s-t <vickneswari.s.t@gmail.com>
2024-03-22 16:36:37 +0530

    date stamp task 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const PORT = 8000
+// const http = require('http')
+const fs = require('fs')
+const express = require('express')
+const app = express()
+
+app.use('/', (req, res) => {
+  try {
+    let today = new Date().getTime()
+    fs.writeFileSync(`date Stamp/${today}.txt`, `${today}`, 'utf8')
+    let data = fs.readFileSync(`date Stamp/${today}.txt`, 'utf8')
+    res.send(data)
+  }
+  catch (error) {
+  }
+})
+app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`))
